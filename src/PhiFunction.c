0094e9993b4110ef5e66a904c88c7c47d1d4a197
EmeraldWolfram <stevenfgy@yahoo.com>
2015-12-14 23:15:28 +0800

    Simply added documentation

--- src/PhiFunction.c
+++ src/PhiFunction.c
@@ -6,7 +6,24 @@
 Expression* getPhiFunction(){
   Expression* phiFunction = NULL;
   
-  phiFunction = createExpression('x', 3, PHI_FUNC, 1, 2, 1);
+  //Argument brought in InputNode
+  //Get the condition from the imdDom
+  //Get the imdDom
+  //TRAVELING TASK: Check modified Id
+  //TRAVELING TASK: Get the oprd1 & oprd2
+  
+  // LinkedList* idList       = getModifiedId(Node* domNode, Node* checkNode);
+  // ListElement* idElement   = idList->head;
+  // while(idList != NULL){
+    // char modID = idElement->node;
+    
+    
+    //                            modID
+    //                              |
+    phiFunction = createExpression('x', 3, PHI_FUNC, 1, 2, 1);
+    // idElement = idElement->next;
+  // }
+  
   
   return phiFunction;
 }
